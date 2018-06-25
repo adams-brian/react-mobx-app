50b6251c65f7c2e8ba908a6efe93392d8d3b3c70
Brian Adams <brian.lives.outdoors@gmail.com>
2018-06-25 17:21:59 -0400

    add first unit test

---
+++ src/components/counter.test.tsx
@@ -0,0 +1,57 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import * as sinon from 'sinon';
+
+import { Counter } from './counter';
+
+const counter = 123;
+const index = 456;
+let increment: sinon.SinonSpy;
+let decrement: sinon.SinonSpy;
+let reset: sinon.SinonSpy;
+let remove: sinon.SinonSpy;
+let element: JSX.Element;
+
+beforeEach(() => {
+  increment = sinon.fake();
+  decrement = sinon.fake();
+  reset = sinon.fake();
+  remove = sinon.fake();
+  element = (
+    <Counter
+      counter={counter}
+      index={index}
+      increment={increment}
+      decrement={decrement}
+      reset={reset}
+      remove={remove}
+     />
+  );
+});
+
+describe("counter", () => {
+  it('renders as expected', () => {
+    const component = shallow(element);
+    expect(component.html()).toMatchSnapshot();
+  });
+  it('calls remove prop function with index', () => {
+    expect(remove.called).toBe(false);
+    shallow(element).find('button').at(0).simulate('click');
+    expect(remove.calledOnceWithExactly(index)).toBe(true);
+  });
+  it('calls increment prop function with index', () => {
+    expect(increment.called).toBe(false);
+    shallow(element).find('button').at(1).simulate('click');
+    expect(increment.calledOnceWithExactly(index)).toBe(true);
+  });
+  it('calls decrement prop function with index', () => {
+    expect(decrement.called).toBe(false);
+    shallow(element).find('button').at(2).simulate('click');
+    expect(decrement.calledOnceWithExactly(index)).toBe(true);
+  });
+  it('calls reset prop function with index', () => {
+    expect(reset.called).toBe(false);
+    shallow(element).find('button').at(3).simulate('click');
+    expect(reset.calledOnceWithExactly(index)).toBe(true);
+  });
+});
